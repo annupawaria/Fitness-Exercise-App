c3abfd3adf934ac8fe7c3842807a29e5daf20378
annupawaria <134483932+annupawaria@users.noreply.github.com>
2024-03-24 15:43:53 +0530

    first commit

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -5,7 +5,7 @@ import { Button, Stack, Typography } from '@mui/material';
 const ExerciseCard = ({ exercise }) => (
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-    {/* <Stack direction="row">
+    <Stack direction="row">
       <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.bodyPart}
       </Button>
@@ -15,7 +15,7 @@ const ExerciseCard = ({ exercise }) => (
     </Stack>
     <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
       {exercise.name}
-    </Typography> */}
+    </Typography>
   </Link>
 );
 
